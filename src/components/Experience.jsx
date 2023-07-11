a54d4fd529dbd9b602b139c139efa724ca942313
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-07-11 15:33:19 +0200

    Update Experience.jsx
    avatar loaded

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,12 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
-      </mesh>
+      <Avatar />
+      <ambientLight intensity={1} />
     </>
   );
 };
