26eefe45b7212cd94d63ede8be821a49a18d0519
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-07-12 21:13:12 +0200

    Update Avatar.jsx
    Animation transition

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -42,9 +42,9 @@ export function Avatar(props) {
 
   // Ejecucion y reseteo de la animacion
   useEffect(() => {
-    actions[animation].reset().play();
+    actions[animation].reset().fadeIn(0.5).play();
     return () => {
-      actions[animation].reset().stop();
+      actions[animation].reset().fadeOut(0.5);
     }
   }, [animation])
 
