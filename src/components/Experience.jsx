ddebe7bba60753be1434f8ed77bd1e82f4ac9082
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-07-12 21:48:22 +0200

    Update Experience.jsx
    Floor and plane

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -19,6 +19,16 @@ export const Experience = () => {
       <Environment preset="sunset" />
       <group position-y={-1}>
         <Avatar animation={animation} />
+        {animation === "Typing" && (
+          <mesh scale={[0.8,0.5,0.8]} position-y={0.25}>
+          <boxGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        )}
+        <mesh scale={5} rotation-x={-Math.PI * 0.5}>
+          <planeGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
       </group>
       {/* <ambientLight intensity={1} /> */}
     </>
