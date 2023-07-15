d6795666cb59c498d642c89a7b084ef5e3180c2f
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-07-15 12:37:40 +0200

    Update Interface.jsx
    section animation

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,12 +1,31 @@
+import { motion } from "framer-motion";
+
 const Section = (props) => {
     const { children } = props;
 
     return (
-        <section className={`h-screen w-screen p-8 max-w-screen-2xl mx-auto flex flex-col items-start justify-center`}>
+        <motion.section
+            className={`
+    h-screen w-screen p-8 max-w-screen-2xl mx-auto
+    flex flex-col items-start justify-center
+    `}
+            initial={{
+                opacity: 0,
+                y: 50,
+            }}
+            whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                    duration: 1,
+                    delay: 0.6,
+                },
+            }}
+        >
             {children}
-        </section>
-    )
-}
+        </motion.section>
+    );
+};
 
 
 export const Interface = () => {
@@ -45,27 +64,27 @@ const AboutSection = () => {
 
 const skills = [
     {
-        title:"Teaching",
+        title: "Teaching",
         level: 90,
     },
     {
-        title:"Rhinoceros 3D",
+        title: "Rhinoceros 3D",
         level: 90,
     },
     {
-        title:"Grasshopper",
+        title: "Grasshopper",
         level: 90,
     },
     {
-        title:"Digital Fabrication",
+        title: "Digital Fabrication",
         level: 90,
     },
     {
-        title:"Threejs / React Three Fiber",
+        title: "Threejs / React Three Fiber",
         level: 70,
     },
     {
-        title:"Web Development",
+        title: "Web Development",
         level: 60,
     },
 ];
@@ -127,47 +146,47 @@ const SkillsSection = () => {
 
 const ContactSection = () => {
     return (
-      <Section>
-        <h2 className="text-5xl font-bold px-10">Get in touch</h2>
-        <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
-          <form>
-            <label for="name" className="font-medium text-gray-900 block mb-1">
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-            />
-            <label
-              for="email"
-              className="font-medium text-gray-900 block mb-1 mt-8"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-            />
-            <label
-              for="email"
-              className="font-medium text-gray-900 block mb-1 mt-8"
-            >
-              Message
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-            />
-            <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
-              Submit
-            </button>
-          </form>
-        </div>
-      </Section>
+        <Section>
+            <h2 className="text-5xl font-bold px-10">Get in touch</h2>
+            <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
+                <form>
+                    <label for="name" className="font-medium text-gray-900 block mb-1">
+                        Name
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+                    />
+                    <label
+                        for="email"
+                        className="font-medium text-gray-900 block mb-1 mt-8"
+                    >
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+                    />
+                    <label
+                        for="email"
+                        className="font-medium text-gray-900 block mb-1 mt-8"
+                    >
+                        Message
+                    </label>
+                    <textarea
+                        name="message"
+                        id="message"
+                        className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+                    />
+                    <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </Section>
     );
-  };
+};
