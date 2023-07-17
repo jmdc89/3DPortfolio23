99de96655c65898199a1f2fe3f52d3ddf32ef950
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-07-17 18:16:40 +0200

    Menu
    structure and css

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       </Scroll>
       </ScrollControls>
     </Canvas>
-    <Menu onSectionChange={setSection} menuOpen={menuOpened} setMenuOpened={setMenuOpened}/>
+    <Menu onSectionChange={setSection} menuOpened={menuOpened} setMenuOpened={setMenuOpened}/>
     </>
   );
 }
