bdd2f690df1ff3aad14dc2dea1296fbb9ecf713a
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-07-18 13:26:06 +0200

    Update Experience.jsx
    section props

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -32,7 +32,7 @@ export const Experience = (props) => {
         <ContactShadows opacity={0.42} scale={10} blur={1} far={10} resolution={256} color="#000000" />
         <Avatar animation={animation} />
         {animation === "Typing" && (
-          < Deskv />
+          < Deskv section={section}/>
         )}
         {animation === "Typing" && (
           <mesh scale={[0.8,0.5,0.8]} position-y={0.25}>
