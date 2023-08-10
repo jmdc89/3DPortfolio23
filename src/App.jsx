3ea055bf046261bc96f4e4e731d87981796e0a6c
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-08-10 14:10:44 -0600

    Update App.jsx
    prop

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
       <ScrollControls pages={4} damping={0.1}>
       <ScrollManager section={section} onSectionChange={setSection}/>
       <Scroll>
-      <Experience section={section}/>
+      <Experience section={section} menuOpened={menuOpened}/>
       </Scroll>
       <Scroll html>
         <Interface />
