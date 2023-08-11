bf4bbf4c5eba99c0214a131a2b7ca3e5a698d504
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-08-11 14:33:18 -0600

    Update Experience.jsx
    camera fix

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { ContactShadows, Environment, OrbitControls, Sky } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls, Sky, useScroll } from "@react-three/drei";
 import {
   Float,
   MeshDistortMaterial,
@@ -10,13 +10,16 @@ import { Deskv } from "./Officemacdesk2";
 import { motion } from "framer-motion-3d";
 import { useThree, useFrame } from "@react-three/fiber";
 import { useMotionValue, animate } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { framerMotionConfig } from "../config";
 import * as THREE from "three";
 
 export const Experience = (props) => {
-  const { section, menuOpened } = props;
+  const {menuOpened } = props;
   const { viewport } = useThree();
+  const data = useScroll();
+
+  const [section, setSection] = useState(0);
 
   const cameraPositionX = useMotionValue();
   const cameraLookAtX = useMotionValue();
@@ -32,7 +35,26 @@ export const Experience = (props) => {
 
   const characterContainerAboutRef = useRef();
 
+  const [characterAnimation, setCharacterAnimation] = useState("Typing");
+  useEffect(() => {
+    setCharacterAnimation("Falling");
+    setTimeout(() => {
+      setCharacterAnimation(section === 0 ? "Typing" : "Standing");
+    }, 600);
+  }, [section]);
+
   useFrame((state) => {
+    let curSection = Math.floor(data.scroll.current * data.pages);
+
+    if (curSection > 3) {
+      curSection = 3;
+    }
+
+    if (curSection !== section) {
+      setSection(curSection);
+    }
+
+
     state.camera.position.x = cameraPositionX.get();
     state.camera.lookAt(cameraLookAtX.get(), 0, 0);
 
@@ -52,8 +74,47 @@ export const Experience = (props) => {
 
   return (
     <>
-      <motion.group position={[0.025658745714699907, -0.08, 2.154407346872966]}>
-        <Avatar animation={section === 0 ? "Falling" : "Standing"} />
+      <motion.group 
+        position={[0.025658745714699907, -0.08, 2.154407346872966]}
+         animate={ "" +  section}
+         transition={{
+           duration: 0.6,
+         }}
+         variants={{
+           0: {
+             scaleX: 1,
+             scaleY: 1,
+             scaleZ: 1,
+             y: 0,
+             rotateY: 0.5,
+           },
+           1: {
+             y: -viewport.height -0.4,
+             x: 0,
+             z: 2,
+             rotateX: 0,
+             rotateY: 0,
+             rotateZ: 0,
+           },
+           2: {
+             x: -2,
+             y: -viewport.height * 2 + 0.5,
+             z: 0,
+             rotateX: 0,
+             rotateY: Math.PI / 2,
+             rotateZ: 0,
+           },
+           3: {
+             y: -viewport.height * 3 + 1,
+             x: 0.3,
+             z: 0,
+             rotateX: 0,
+             rotateY: -Math.PI / 4,
+             rotateZ: 0,
+           },
+         }}
+      >
+        <Avatar animation={characterAnimation} />
       </motion.group>
 
 
@@ -64,16 +125,27 @@ export const Experience = (props) => {
         position-y={0}
         position-z={2}
         rotation-y={Math.PI / 5}
-      // animate={{
-      //   y: section === 0 ? 0 : -0.5,
-      // }}
+        animate={{
+          y: section === 0 ? 0 : -0.5,
+        }}
       >
-        <group ref={characterContainerAboutRef}  name="Empty" position={[-0.07, -0.08, 0.14]} rotation={[-Math.PI, 0.04, -Math.PI]} scale={0.25}>
+        < Deskv section={section} />
+        <group 
+          ref={characterContainerAboutRef}  
+          name="Empty" position={[-0.07, -0.08, 0.14]} 
+          rotation={[-Math.PI, 0.04, -Math.PI]} 
+          // scale={4}
+          ></group>
+          {/* /* <group scale={[1.5, 1.5, 1.5]} position-y={-1.5}>
+            <Avatar animation={section === 0 ? "Falling" : "Standing"} />
+          </group> */}
+        
+        {/* <group ref={characterContainerAboutRef}  name="Empty" position={[-0.07, -0.08, 0.14]} rotation={[-Math.PI, 0.04, -Math.PI]} scale={0.25}> */}
           {/* <group scale={[1.5, 1.5, 1.5]} position-y={-1.5}>
             <Avatar animation={section === 0 ? "Falling" : "Standing"} />
           </group> */}
-        </group>
-        <ContactShadows opacity={0.42} scale={10} blur={1} far={10} resolution={256} color="#000000" />
+        {/* </group> */}
+        {/* <ContactShadows opacity={0.42} scale={10} blur={1} far={10} resolution={256} color="#000000" />
         <Avatar animation={animation} />
         {animation === "Typing" && (
           < Deskv section={section} />
@@ -82,8 +154,12 @@ export const Experience = (props) => {
           <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
             <boxGeometry />
             <meshStandardMaterial color="blue" />
+          </mesh> */}
+        {/* )} */}
+          <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+            <boxGeometry />
+            <meshStandardMaterial color="blue" />
           </mesh>
-        )}
         <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
           <planeGeometry />
           <meshStandardMaterial color="orange" />
