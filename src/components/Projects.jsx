64d7b0529ed6c39786110e64ec4c3da6e8cd400d
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-08-12 17:14:47 -0600

    Update Projects.jsx
    projects list

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,37 @@
+export const projects = [
+    {
+      title: "Netflix clone",
+      url: "https://netflixcloneui.netlify.app/",
+      image: "projects/netflixclone.JPG",
+      description: "This app is a netflix clone built using react",
+    },
+    {
+      title: "Robot Picker",
+      url: "https://talerobot.netlify.app/",
+      image: "projects/robotpicker.JPG",
+      description: "This is a small primer on how to use GLTF models on the web",
+    },
+    {
+      title: "React Micro projects",
+      url: "https://react-microprojects.netlify.app/",
+      image: "projects/microProjects.JPG",
+      description: "Microprojects developed using React",
+    },
+    {
+      title: "Tesla UI Clone",
+      url: "https://teslauiclone.netlify.app/",
+      image: "projects/teslauiclone.jpg",
+      description: "Tesla UI Homepage clone built using react",
+    },
+    {
+      title: "Portfolio v1",
+      url: "https://portwebsitev1.netlify.app/",
+      image: "projects/portfoliov1.JPG",
+      description: "First version of my portfolio",
+    },
+  ];
+  
+
 export const Projects = () => {
     return <group></group>;
 };
