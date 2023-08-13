85c430d5dbc2914a3d671e10cc20a322412d73c1
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-08-13 12:31:10 -0600

    Update Projects.jsx
    Project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,10 @@
-import { useThree } from "@react-three/fiber";
-import {motion } from "framer-motion-3d";
+import { Image, Text } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { animate, useMotionValue } from "framer-motion";
+
+import { motion } from "framer-motion-3d";
+// import { atom, useAtom } from "jotai";
+import { useEffect, useRef } from "react";
 
 export const projects = [
     {
@@ -23,7 +28,7 @@ export const projects = [
     {
       title: "Tesla UI Clone",
       url: "https://teslauiclone.netlify.app/",
-      image: "projects/teslauiclone.jpg",
+      image: "projects/teslauiclone.JPG",
       description: "Tesla UI Homepage clone built using react",
     },
     {
@@ -33,14 +38,71 @@ export const projects = [
       description: "First version of my portfolio",
     },
   ];
-  
 
+  const Project = (props) => {
+    const { project, highlighted } = props;
+  
+    const background = useRef();
+    const bgOpacity = useMotionValue(0.4);
+  
+    useEffect(() => {
+      animate(bgOpacity, highlighted ? 0.7 : 0.4);
+    }, [highlighted]);
+  
+    useFrame(() => {
+      background.current.material.opacity = bgOpacity.get();
+    });
+  
+    return (
+      <group {...props}>
+        <mesh
+          position-z={-0.001}
+          onClick={() => window.open(project.url, "_blank")}
+          ref={background}
+        >
+          <planeGeometry args={[2.2, 2]} />
+          <meshBasicMaterial color="black" transparent opacity={0.4} />
+        </mesh>
+        <Image
+          scale={[2, 1.2, 1]}
+          url={project.image}
+          toneMapped={false}
+          position-y={0.3}
+        />
+        <Text
+          maxWidth={2}
+          anchorX={"left"}
+          anchorY={"top"}
+          fontSize={0.2}
+          position={[-1, -0.4, 0]}
+        >
+          {project.title.toUpperCase()}
+        </Text>
+        <Text
+          maxWidth={2}
+          anchorX="left"
+          anchorY="top"
+          fontSize={0.1}
+          position={[-1, -0.6, 0]}
+        >
+          {project.description}
+        </Text>
+      </group>
+    );
+  };
+  
+  
 export const Projects = () => {
-    return <group position-y={-viewport.height * 2 + 1}>
+  const { viewport } = useThree();
+
+    return <group position-y={-viewport.height * 2 - 1.5}>
       {
         projects.map((project,index) => (
-          <motion.group>
-            <Project />
+          <motion.group 
+            key={"project_" + index}
+            position={[index * 2.5, 0, -3]}
+          >
+            <Project project={project}/>
           </motion.group>
         ))
       }
